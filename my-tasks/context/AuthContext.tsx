6eaf201b1caf744b3618bc97f89a9244b9512b32
muziga judith <umuzigajudit@gmail.com>
2025-11-13 09:14:25 +0200

    improved the path to the firebase

--- my-tasks/context/AuthContext.tsx
+++ my-tasks/context/AuthContext.tsx
@@ -3,7 +3,8 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import type { ReactNode } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
-import { auth } from "../lib/firebase";
+import { auth } from "@/lib/firebase";
+// import { auth } from "../lib/firebase";
 
 interface AuthContextType {
   user: User | null;
